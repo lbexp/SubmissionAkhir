430ab9d06a10044f29cbc3da0d73dc355ffeb235
Fiqar Aprialim <fiqarapr@gmail.com>
2021-12-19 02:11:02 +0800

    Initial Commit

---
+++ SubmissionAkhir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmissionAkhir
+//
+//  Created by Fiqar Aprialim on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
